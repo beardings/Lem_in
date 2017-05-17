9f38fa9fb5411a642a7c6d7fe5828ba308d5e351
mponomar <beardings@gmail.com>
2017-05-17 18:31:49 +0300

    start

---
+++ lemin_valid.c
@@ -0,0 +1,10 @@
+//
+// Created by Mykola Ponomarov on 17.05.17.
+//
+
+#include "lem_in.h"
+
+int lemin_valid(char *str)
+{
+    return (0);
+}
--- main.c
+++ main.c
@@ -2,7 +2,20 @@
 // Created by Mykola Ponomarov on 17.05.17.
 //
 
-int main(void)
+#include "lem_in.h"
+
+int main(int c, char **v)
 {
+    char *line;
+    int fd;
+
+    line = NULL;
+    if (c == 2)
+    {
+        fd = open(v[1], O_RDONLY);
+        while ((get_next_line(fd, &line)))
+            lemin_valid(line);
+        close(fd);
+    }
     return (0);
 }
